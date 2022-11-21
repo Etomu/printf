21e9654809191947d6ea4a65b23deeb635173466
KingBaba008 <113688270+KingBaba008@users.noreply.github.com>
2022-11-21 22:15:40 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * handle_print - Printa an argument based on its type
- * @fmt: formatted string in which to print the arguments.
- * @list: List of argument to be printed.
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
@@ -11,13 +11,14 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
-int handle_print(const char *fmt, int *ind,va_list list, char buffer[],
-		int flags, int width, int percision, int size)
-{int i, unknow_len = 0, printed_chars = -1;
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecima},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
@@ -42,7 +43,7 @@ int handle_print(const char *fmt, int *ind,va_list list, char buffer[],
 			return (1);
 		}
 		unknow_len += write(1, &fmt[*ind], 1);
-		return (unkown_len);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
