b7adfeca4192f411394de5722f03c80fdb097275
KingBaba008 <113688270+KingBaba008@users.noreply.github.com>
2022-11-21 22:13:46 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -8,7 +8,7 @@
  */
 int is_printable(char c)
 {
-	if (c = 32 && c < 127)
+	if (c >= 32 && c < 127)
 		return (1);
 
 	return (0);
@@ -41,11 +41,11 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
  *
- * REturn: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int is_digit(char c)
 {
-	if (c >= '0'&& c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 
 	return (0);
@@ -70,7 +70,7 @@ long int convert_size_number(long int num, int size)
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted.
+ * @num: Number to be casted
  * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
